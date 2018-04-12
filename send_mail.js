7e12fd7639502e5e57031a7d586c183338d2e496
Marco Gasparini <Gappa@MacBook-Pro-di-Gappa.local>
2018-04-12 22:18:18 +0200

    api

--- send_mail.js
+++ send_mail.js
@@ -1,7 +1,7 @@
 // using SendGrid's v3 Node.js Library
 // https://github.com/sendgrid/sendgrid-nodejs
 const sgMail = require('@sendgrid/mail');
-sgMail.setApiKey('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');
+
 
 module.exports.send_mail = function (to, subject, body, html) {
     const msg = {
