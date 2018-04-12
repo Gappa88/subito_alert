e48974cf8f0f9206d42b30a7027391c3f5bf7996
Gappa88 <gappa88@gmail.com>
2018-04-12 21:38:26 +0200

    Update main.js
    paste

--- main.js
+++ main.js
@@ -19,15 +19,49 @@ for (let k in res_tmp) {
 
 const insertions_interval_checker_seconds = nconf.get("insertions_interval_checker_seconds");
 
-subito.start(researches);
- setInterval(function () {
-   subito.start(researches);
- }, 1000 * insertions_interval_checker_seconds);
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 8080;
 
-
- var express = require('express');
- var app = express();
- var port = process.env.PORT || 8080;
- app.listen(port, function() {
+app.listen(port, function () {
   console.log('Our app is running on http://localhost:' + port);
 });
+
+app.get('/', function(req, res) {
+  res.send('hello');
+});
+
+
+
+var http = require('http'); //importing http
+
+function startKeepAlive() {
+  setInterval(function () {
+    var options = {
+      host: 'still-dusk-84428.herokuapp.com',
+      port: 80,
+      path: '/'
+    };
+    http.get(options, function (res) {
+      res.on('data', function (chunk) {
+        try {
+          // optional logging... disable after it's working
+          console.log("HEROKU RESPONSE: " + chunk);
+        } catch (err) {
+          console.log(err.message);
+        }
+      });
+    }).on('error', function (err) {
+      console.log("Error: " + err.message);
+    });
+  }, 20 * 60 * 1000); // load every 20 minutes
+}
+
+startKeepAlive();
+
+console.log("attivo ogni: " + insertions_interval_checker_seconds);
+
+subito.start(researches);
+setInterval(function () {
+  subito.start(researches);
+}, 1000 * insertions_interval_checker_seconds);
