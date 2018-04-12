38c10461e96b2c6064eb1c34f5088823956cc702
Gappa88 <gappa88@gmail.com>
2018-04-12 21:42:29 +0200

    Create db_manager.js
    db manager

---
+++ db_manager.js
@@ -0,0 +1,158 @@
+const { Client } = require('pg');
+const sqlite3 = require('sqlite3').verbose();
+
+var db = null;
+
+
+async function close() {
+    //db.close();
+    await db.end();
+    db = null;
+    return ;
+}
+
+
+// controllo presenza tabelle
+// function create_db() {
+//     return new Promise((resolve, reject) => {
+//         let db = new sqlite3.Database('./urls.db', (err) => {
+//             if (err) {
+//                 return reject(err);
+//             }
+//             console.log('Connected to the urls database.');
+//             return resolve(db);
+//         });
+//     });
+// }
+async function create_db() {
+    db = new Client({
+        connectionString: process.env.DATABASE_URL,
+        ssl: true,
+    });
+    return db.connect();
+}
+
+// function create_table_insertions() {
+//     return new Promise((resolve) => {
+//         try {
+//             db.serialize(function () {
+//                 let sql_urls = `CREATE TABLE IF NOT EXISTS insertions (
+//                     id integer PRIMARY KEY,
+//                     id_research integer not null, 
+//                     data_id text,
+//                     url text,
+//                     description text,
+//                     price text,
+//                     location text,
+//                     extras text,
+//                     updated_at	integer
+//                 )`;
+//                 db.run(sql_urls);
+//                 db.run("CREATE UNIQUE INDEX IF NOT EXISTS index_data_id ON insertions(data_id)");
+//                 return resolve();
+//             });
+//         } catch (err) {
+//             log.error(err);
+//         }
+//     });
+// }
+function create_table_insertions() {
+    let sql_urls = `CREATE TABLE IF NOT EXISTS insertions (
+        id serial PRIMARY KEY,
+        id_research integer not null, 
+        data_id text,
+        url text,
+        description text,
+        price text,
+        location text,
+        extras text,
+        updated_at bigint
+       ); 
+       CREATE UNIQUE INDEX IF NOT EXISTS index_data_id ON insertions(data_id)`;
+
+    return db.query(sql_urls);
+}
+
+// function get_insertions_by_research_id(id) {
+//     return new Promise((resolve, reject) => {
+//         let sql_urls = `select * from insertions
+//     where id_research = ` + id;
+
+//         db.all(sql_urls, [], (err, rows) => {
+//             if (err) {
+//                 return reject(err);
+//             }
+//             return resolve(rows);
+//         });
+//     });
+// }
+
+async function get_insertions_by_research_id(id) {
+    let sql_urls = `select * from insertions where id_research = ` + id;
+    let rows = await db.query(sql_urls);
+    return rows.rows;
+}
+
+
+// function insert_insertion_or_update(ins_obj) {
+//     return new Promise((resolve, reject) => {        
+//         let sql_urls = `INSERT OR REPLACE into insertions (id_research, data_id, url, description, price, location, extras, updated_at)
+//     values(?,?,?,?,?,?,?,?)`;
+
+//         db.run(sql_urls, [
+//             ins_obj.id_research,
+//             ins_obj.data_id,
+//             ins_obj.url,
+//             ins_obj.description,
+//             ins_obj.price,
+//             ins_obj.location,
+//             ins_obj.extras,
+//             now
+//         ], (err, rows, bo) => {
+//             if (err) {
+//                 return resolve(err);
+//             }
+//             return resolve();
+//         });
+//     });
+// }
+async function insert_insertion_or_update(ins_obj) {
+    let sql_urls = `INSERT into insertions (id_research, data_id, url, description, price, location, extras, updated_at)
+    values($1,$2,$3::text,$4::text,$5::text,$6::text,$7::text,$8)
+    on conflict(data_id)
+    DO UPDATE SET 
+    url = $3::text, 
+    description = $4::text, 
+    price = $5::text,
+    location = $6::text,
+    extras = $7::text,
+    updated_at = $8
+    where insertions.data_id = $2
+    `;
+
+    //try {
+        //let aa = await
+         return db.query(sql_urls, [
+            ins_obj.id_research,
+            ins_obj.data_id,
+            ins_obj.url,
+            ins_obj.description,
+            ins_obj.price,
+            ins_obj.location,
+            ins_obj.extras,
+            ins_obj.updated_at
+        ]);
+    // } catch (err) {
+    //     console.log(err);
+    //     console.log(ins_obj);
+    //     console.log(sql_urls);
+    // }
+}
+
+module.exports = {
+    create_db: create_db,
+    create_table_insertions: create_table_insertions,
+    get_insertions_by_research_id: get_insertions_by_research_id,
+    insert_insertion_or_update: insert_insertion_or_update,
+    close: close
+};
