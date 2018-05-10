0a73e7fa4fff42a16d960b42f752cb4ed760c211
Marco Gasparini <Gappa@MacBook-Pro-di-Gappa.local>
2018-05-10 23:57:49 +0200

    send

--- main.js
+++ main.js
@@ -3,19 +3,19 @@ const nconf = require('nconf');
 const { createLogger, format, transports } = require('winston');
 const { combine, timestamp, label, printf } = format;
 const myFormat = printf(info => {
-    return `${info.timestamp} ${info.level}: ${info.message}`;
+  return `${info.timestamp} ${info.level}: ${info.message}`;
 });
 const log = createLogger({
   format: combine(
-      timestamp(),
-      myFormat
+    timestamp(),
+    myFormat
   ),
   transports: [
-      new transports.Console(),
-      new transports.File({
-          filename: 'scraping_subito.log',
-          level: 'info'
-      })]
+    new transports.Console(),
+    new transports.File({
+      filename: 'scraping_subito.log',
+      level: 'info'
+    })]
 });
 
 nconf.file("config.json");
@@ -38,11 +38,11 @@ for (let k in res_tmp) {
 
 
 
-// for (let r in researches) {
-//   let scraper = new subito(log);
-//   scraper.start(researches[r]);
-//   setInterval(function () {    
-//     let scraper2 = new subito(log);
-//     scraper2.start(researches[r]);
-//   }, researches[r].insertions_interval_checker_seconds * 1000);
-// }
+for (let r in researches) {
+  let scraper = new subito(log);
+  scraper.start(researches[r]);
+  setInterval(function () {    
+    let scraper2 = new subito(log);
+    scraper2.start(researches[r]);
+  }, researches[r].insertions_interval_checker_seconds * 1000);
+}
